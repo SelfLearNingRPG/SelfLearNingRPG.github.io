f432fdb5a15c3292693d201279c33baae91bd2a9
94SungLA <102648382+SelfLearNingRPG@users.noreply.github.com>
2022-04-13 15:39:01 +0800

    Update game.js

--- game.js
+++ game.js
@@ -29,9 +29,9 @@ var player_Id = "大中天";
 
 function btclick() {
     player_Id = prompt("取個名字：", "大中天");
+    window.location.href = "page2.html";
 }
 
-function stratgame() {
-    //window.location.href = "page2.html";
+function startgame() {
     document.getElementById('output1').innerHTML = "這裡是你旅途的起點....<br><你出賣了你的靈魂><br>世界之聲: 你好，我是作者大人的其中一部分，你的靈魂我確實收下了。<br>現在就將用" + player_Id + "的靈魂換來的地下城挑戰許可附加在你身上…雖然我本來想這麼說，<br>但在此之前，你還不是冒險者對吧？這樣就想挑戰地下城簡直是不可能，<br>先去選個職業，有了初級的戰力後再來見我吧~。";
 }
