f3bfa458b2251b7757645117be53b63092794f31
94SungLA <102648382+SelfLearNingRPG@users.noreply.github.com>
2022-04-17 07:00:31 +0800

    Update game.js

--- game.js
+++ game.js
@@ -2,8 +2,9 @@ function disp_alert() {
     alert("因為作者之力強制你要去當冒險者，你也無法拒絕，\n快去選擇你的職業吧！。")
 }
 
+var player_Id = '';
 function btclick() {
-    var player_Id = prompt("取個名字：", "大中天");
+    player_Id = prompt("取個名字：", "大中天");
     window.location.href = "page2.html";
 }
 
