94cb3aec51ecc5089c7f26a828252aae81a7193b
SelfLearNingRPG <11002selflearning@gmail.com>
2022-05-18 15:17:27 +0800

    update

--- game.js
+++ game.js
@@ -11,7 +11,7 @@ function btclick() {
 
 function startgame() {
     player_Id = document.cookie.replace(/(?:(?:^|.*;\s*)player_Id\s*\=\s*([^;]*).*$)|^.*$/, "$1");
-    document.getElementById('output1').innerHTML = "這裡是你旅途的起點....<br>???：<br>你好，" + player_Id + "，恭候多時，我是作者大人的其中一部分，你的請求我已經收到了。<br>現在就將刻有" + player_Id + "的地下城鑰匙交給你....<br>雖然我本來想這麼說，但在此之前，你還沒有任何能力，這樣就想挑戰地下城簡直是白白送死，<br>先去選個職業，然後踏上變強的道路吧!<br>等時機成熟，我會再次顯現在你面前<br>祝你一切順利。<br> < img src = \"images/introduction.jpeg\" width = \"80%\" >";
+    document.getElementById('output1').innerHTML = "這裡是你旅途的起點....<br>???：<br>你好，" + player_Id + "，恭候多時，我是作者大人的其中一部分，你的請求我已經收到了。<br>現在就將刻有" + player_Id + "的地下城鑰匙交給你....<br>雖然我本來想這麼說，但在此之前，你還沒有任何能力，這樣就想挑戰地下城簡直是白白送死，<br>先去選個職業，然後踏上變強的道路吧!<br>等時機成熟，我會再次顯現在你面前<br>祝你一切順利。<br> ";
 }
 var mp = 0;
 var hp = 0;
@@ -97,14 +97,18 @@ function resetvalue() {
 
 }
 
-var pro_index = ["knight", "ranger", "magician"];
-var pro_items = ["騎士", "遊俠", "法師"];
+
+
 function begin() {
-    for (var i = 0; i < 3; i++) {
-        if (pro = pro_index[i]) {
-            pro = pro_items[i];
-            break;
-        }
+    var zy;
+    if(pro == "knight"){
+        zy = "騎士";
     }
-    document.getElementById('begin').innerHTML = "你是一個" + pro + "<br>";
+    else if(pro == "ranger"){
+        zy = "遊俠";
+    }
+    else if(pro == "magician"){
+        zy = "法師";
+    }
+    document.getElementById('begin').innerHTML = "你是一個" + zy + "<br>";
 }
